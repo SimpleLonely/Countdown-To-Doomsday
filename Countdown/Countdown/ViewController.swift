1bb19f69c8b2d452e4f5610798d8150b3f854123
Lin-from-iMac <710282573@qq.com>
2018-11-21 22:06:55 +0800

    initial main UI with nothing

---
+++ Countdown/Countdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Countdown
+//
+//  Created by apple on 2018/11/21.
+//  Copyright © 2018年 nju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
